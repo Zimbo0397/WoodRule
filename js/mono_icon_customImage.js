d9611dbf7190ae97e357ecaad41e8312064902b0
zimbo0397 <zbryzskiy.intertech@gmail.com>
2016-01-29 10:54:46 +0200

    init

---
+++ js/mono_icon_customImage.js
@@ -0,0 +1,26 @@
+ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
+       center: [55.74954, 37.621587],
+       zoom: 10,
+       controls: []
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+            hintContent: 'Собственный значок метки',
+            balloonContent: 'Это красивая метка'
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#image',
+            // Своё изображение иконки метки.
+            iconImageHref: 'img/icons/trick-pin.png',
+            // Размеры метки.
+            iconImageSize: [0, 0],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-3, -42]
+        });
+
+    myMap.geoObjects.add(myPlacemark);
+});
