09e3a7e0862dd91eb9b90501c3a97d3135f49469
zimbo0397 <zbryzskiy.intertech@gmail.com>
2016-02-02 20:30:26 +0200

    02.02.2016

--- js/vendor/demo.js
+++ js/vendor/demo.js
@@ -10,13 +10,8 @@
  */
 
 /* global blueimp, $ */
-
-$(function () {
-    'use strict';
-
-
-    // Initialize the Gallery as video carousel:
-    var myGal = blueimp.Gallery([
+/*
+[
         {
             title: 'LES TWINS - An Industry Ahead',
             type: 'text/html',
@@ -42,17 +37,49 @@ $(function () {
             type: 'text/html',
             youtube: 'zi4CIXpx7Bg'
         }
-    ], {
+    ]
+*/
+
+$(function () {
+    'use strict';
+
+    var currentVideo = 0,
+        $desc = $('.vodeo-section .desc-holder');
+
+    function prevContent () {
+        if (currentVideo) {
+            currentVideo--;
+            $desc.html(videos[currentVideo].description);
+        }
+    }
+
+    function nextContent () {
+        if (currentVideo < videos.length - 1) {
+            currentVideo++;
+            $desc.html(videos[currentVideo].description);
+        }
+    }
+
+
+    // Initialize the Gallery as video carousel:
+    var myGal = blueimp.Gallery(videos, {
         container: '#blueimp-video-carousel',
-        carousel: true
+        carousel: true,
+        startSlideshow: false
     });
     $('#blueimpPrev').on('click', function (e) {
         e.preventDefault();
-        myGal.prev();
+        if (currentVideo) {
+            myGal.prev();
+        }
+        prevContent();
     })
     $('#blueimpNext').on('click', function (e) {
         e.preventDefault();
-        myGal.next();
+        if (currentVideo < videos.length - 1) {
+            myGal.next();
+        }
+        nextContent();
     })
 
 });
